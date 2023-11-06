fef5c9df74aadf5e43cf94ed2ce24fbbcf0bf625
Brittany Halterman <halterma@gmail.com>
2023-11-05 20:59:23 -0500

    implement route for adding new people entry

--- app.js
+++ app.js
@@ -11,8 +11,55 @@ const app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 
+// People entries are stored in the people array, which starts out empty.
+// They are not persisted.
+// There is no file i/o or database access in this exercise.
 const people = [];
 
+// Routes
+
+// adding a new people entry
+// post request to the URI /api/v1/people
+app.post("/api/v1/people", (req, res) => {
+  // name (string) and age (number) are required
+
+  // if no name in req.body
+  if (!req.body.name) {
+    // return json document with 400 result code and message
+    res.status(400).json({ error: "Please enter name." });
+    return;
+  }
+  // if no age in req.body
+  if (!req.body.age) {
+    // return json document with 400 result code and message 
+    res.status(400).json({ error: "Please enter age." });
+    return;
+  }
+  // age must be non-negative number
+  const age = Number(req.body.age);
+  // if age is NaN (not a number) and/or less than 0
+  if (isNaN(age) || age < 0) {
+    // return json document with 400 result code and message
+    res.status(400).json({ error: "Please enter valid age." })
+  }
+  // create entry
+  req.body.age = age;
+  req.body.index = people.length;
+  people.push(req.body)
+  // if entry is created
+  // return JSON document with a message saying that "A person entry was added" along with the index of the entry just added
+  res.status(201).json({ message: "A person record was added,", index: req.body.index });
+})
+
+//  retrieving the list of people entries
+app.get("/api/v1/people", (req, res) => {
+
+})
+
+// retrieving a single person entry
+app.get("/api/v1/people/:id", (req, res) => {
+
+})
 
 app.all("/api/v1/*", (req, res) => {
   res.json({ error: "That route is not implemented." });
@@ -21,3 +68,4 @@ app.all("/api/v1/*", (req, res) => {
 const server = app.listen(3000, () => {
   console.log("listening on port 3000...");
 });
+
--- public/client.js
+++ public/client.js
@@ -11,20 +11,16 @@ document.addEventListener("DOMContentLoaded", () => {
   addPerson.addEventListener("click", async (event) => {
     event.preventDefault();
     try {
-      // send post request to URI /api/v1/people where body of request is json document containing name (string) and age (number)
       const response = await fetch("/api/v1/people", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        // each entry must have a name and an age
-        body: JSON.stringify({ name: name.value, age: Number(age.value) }), // age must be non-negative
+        body: JSON.stringify({ name: name.value, age: Number(age.value) }),
       });
       const data = await response.json();
-      // return JSON document with a message saying that "A person entry was added" along with the index of the entry just added
       result.textContent = JSON.stringify(data);
     } catch (err) {
-      // return JSON document with an error message with a 400 result code
       result.textContent = err.message;
     }
   });
@@ -33,12 +29,10 @@ document.addEventListener("DOMContentLoaded", () => {
   listPeople.addEventListener("click", async (event) => {
     event.preventDefault();
     try {
-      // get request to the URI /api/v1/people
       const response = await fetch("/api/v1/people", {
         headers: { "Content-Type": "application/json" },
       });
       const data = await response.json();
-      // return JSON document containing the array
       result.textContent = JSON.stringify(data);
     } catch (err) {
       result.textContent = err.message;
@@ -51,16 +45,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const index1 = encodeURIComponent(index.value);
     console.log("index 1 is ", index1);
     try {
-      // get request to /api/v1/people/:id
-      // :id is index of entry to be retrieved
       const response = await fetch(`/api/v1/people/${index1}`, { 
         headers: { "Content-Type": "application/json" },
       });
       const data = await response.json();
-      // return JSON document with the entry
       result.textContent = JSON.stringify(data);
     } catch (err) {
-      // return error message and a 404 result code
       result.textContent = err.message;
     }
   });
